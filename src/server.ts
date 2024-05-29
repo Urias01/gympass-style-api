fe6b06167a345ccf31e23c12ab1ddf92744a3598
Urias Pereira <uriasluis64@gmail.com>
2024-05-29 16:34:08 -0300

    setup environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { app } from "./app";
+import { env } from "./env";
 
 
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log(`HTTP Server Running!`)
 })
