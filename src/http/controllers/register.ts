92706a0e2d72ddb6fca84bffb8f098e32344c045
Urias Pereira <uriasluis64@gmail.com>
2024-05-31 15:52:50 -0300

    password hash and validation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,6 @@
 import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -11,8 +13,20 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    reply.status(409).send()
+  }
+
   await prisma.user.create({
-    data: { name, email, password_hash: password },
+    data: { name, email, password_hash },
   })
 
   reply.status(201).send()
