c94803d330cd4a6d0f3b668a58c83204e0d81230
Urias Pereira <uriasluis64@gmail.com>
2024-05-31 15:58:53 -0300

    register  use case

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,6 @@
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
-
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { registerUseCase } from '../use-cases/regiter'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -13,21 +11,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    reply.status(409).send()
+  try {
+    registerUseCase({ name, email, password })
+  } catch (err) {
+    return reply.status(409).send()
   }
-
-  await prisma.user.create({
-    data: { name, email, password_hash },
-  })
 
   reply.status(201).send()
 }
