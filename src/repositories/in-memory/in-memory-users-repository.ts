af1b8b99dda901d97adcfa799e7b770ba4075368
Urias Pereira <uriasluis64@gmail.com>
2024-06-03 11:28:07 -0300

    in memory database

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,30 @@
+import { Prisma, User } from '@prisma/client'
+import { PrismaUsersRepository } from '../prisma/prisma-users-repository'
+
+export class InMemoryUsersRepository implements PrismaUsersRepository {
+  public item: User[] = []
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.item.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.item.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+}
