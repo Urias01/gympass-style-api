8c3b88d4ab76adb3788e7dc4395ff99dae7db18f
Urias Pereira <uriasluis64@gmail.com>
2024-06-06 10:57:01 -0300

    tdd (test-driven development)  && mokcking

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,6 +1,4 @@
-import { beforeEach, describe, expect, it } from 'vitest'
-import { hash } from 'bcryptjs'
-import { ResourceNotFountError } from './errors/resource-not-fount-error'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 
 import { CheckInUseCase } from './check-in'
@@ -12,10 +10,48 @@ describe('Get User Profile Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
   })
 
-  it('should be able to get user profile with wrong id', async () => {
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to check in', async () => {
     const { checkIn } = await sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+    await sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01',
+    })
+
+    await expect(
+      sut.execute({
+        userId: 'user-01',
+        gymId: 'gym-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to check in twice but in the different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+    await sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01',
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
+    const { checkIn } = sut.execute({
       userId: 'user-01',
       gymId: 'gym-01',
     })
