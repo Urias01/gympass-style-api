b5fea2c9f2675933eb8c6d575293048cd2bac077
Urias Pereira <uriasluis64@gmail.com>
2024-06-11 15:41:37 -0300

    create checkIns controllers

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { usersRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import { gymsRoutes } from './http/controllers/gyms/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
@@ -11,6 +13,8 @@ app.register(fastifyJwt, {
 })
 
 app.register(usersRoutes)
+app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
