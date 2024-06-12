9d6bc235e90b5ba888de5d89bebdc6282544e98b
Urias Pereira <uriasluis64@gmail.com>
2024-06-12 10:21:23 -0300

    fix verify user role

--- src/http/middlewares/verify-user-role.ts
+++ src/http/middlewares/verify-user-role.ts
@@ -5,7 +5,7 @@ export function verifyUserRole(roleToVerify: Role) {
   return async (request: FastifyRequest, reply: FastifyReply) => {
     const { role } = request.user
 
-    if (role === roleToVerify) {
+    if (role !== roleToVerify) {
       return reply.status(401).send({ message: 'Unauthorized' })
     }
   }
